cd830c0f4730317816861430b3328636ff6e0b4c
Alexander Huracan <Nokia2611@mail.ru>
2021-07-04 10:35:14 +0300

    feat: add database for sets

---
+++ english-for-kids-base/src/data/sets.ts
@@ -0,0 +1,112 @@
+export const sets = {
+  actionSetA: [
+    {
+      id: 1,
+      text: 'Cry',
+      translate: 'Плакать',
+      audio: './audio/cry.mp3',
+      image: './images/cry.jpg',
+    },
+    {
+      id: 2,
+      text: 'Dance',
+      translate: 'Танцевать',
+      audio: './audio/dance.mp3',
+      image: './images/dance.jpg',
+    },
+    {
+      id: 3,
+      text: 'Dive',
+      translate: 'Нырять',
+      audio: './audio/dive.mp3',
+      image: './images/dive.jpg',
+    },
+    {
+      id: 4,
+      text: 'Draw',
+      translate: 'Рисовать',
+      audio: './audio/draw.mp3',
+      image: './images/draw.jpg',
+    },
+    {
+      id: 5,
+      text: 'Fishing',
+      translate: 'Рыбачить',
+      audio: './audio/fish.mp3',
+      image: './images/fish.jpg',
+    },
+    {
+      id: 6,
+      text: 'Fly',
+      translate: 'Летать',
+      audio: './audio/fly.mp3',
+      image: './images/fly.jpg',
+    },
+    {
+      id: 7,
+      text: 'Laugh',
+      translate: 'Смеяться',
+      audio: './audio/laugh.mp3',
+      image: './images/laugh.jpg',
+    },
+    {
+      id: 8,
+      text: 'Open',
+      translate: 'Открывать',
+      audio: './audio/open.mp3',
+      image: './images/open.jpg',
+    },
+  ],
+  actionSetB: [
+    {
+      id: 1,
+
+      text: 'Cry',
+      translate: 'Плакать',
+      audio: './audio/cry.mp3',
+      image: './images/cry.jpg',
+    },
+    {
+      text: 'Dance',
+      translate: 'Танцевать',
+      audio: './audio/dance.mp3',
+      image: './images/dance.jpg',
+    },
+    {
+      text: 'Dive',
+      translate: 'Нырять',
+      audio: './audio/dive.mp3',
+      image: './images/dive.jpg',
+    },
+    {
+      text: 'Draw',
+      translate: 'Рисовать',
+      audio: './audio/draw.mp3',
+      image: './images/draw.jpg',
+    },
+    {
+      text: 'Fishing',
+      translate: 'Рыбачить',
+      audio: './audio/fish.mp3',
+      image: './images/fish.jpg',
+    },
+    {
+      text: 'Fly',
+      translate: 'Летать',
+      audio: './audio/fly.mp3',
+      image: './images/fly.jpg',
+    },
+    {
+      text: 'Laugh',
+      translate: 'Смеяться',
+      audio: './audio/laugh.mp3',
+      image: './images/laugh.jpg',
+    },
+    {
+      text: 'Open',
+      translate: 'Открывать',
+      audio: './audio/open.mp3',
+      image: './images/open.jpg',
+    },
+  ],
+};
