47b048e8997d61ec778b3bac38c6bc1dcc204c30
Alexander Huracan <Nokia2611@mail.ru>
2021-07-04 10:35:36 +0300

    feat: add one more property for correct routes

--- english-for-kids-base/src/data/categories.ts
+++ english-for-kids-base/src/data/categories.ts
@@ -1,33 +1,41 @@
 export const categories = [
   {
+    route: 'action-set-a',
     category: 'Action (set A)',
     image: './images/swim.jpg',
   },
   {
+    route: 'action-set-b',
     category: 'Action (set B)',
     image: './images/dive.jpg',
   },
   {
+    route: 'action-set-c',
     category: 'Action (set C)',
     image: './images/dance.jpg',
   },
   {
+    route: 'adjective',
     category: 'Adjective',
     image: './images/homeless.jpg',
   },
   {
+    route: 'animal-set-a',
     category: 'Animal (set A)',
     image: './images/chicken.jpg',
   },
   {
+    route: 'animal-set-b',
     category: 'Animal (set B)',
     image: './images/dog.jpg',
   },
   {
+    route: 'clothes',
     category: 'Clothes',
     image: './images/boot.jpg',
   },
   {
+    route: 'emotions',
     category: 'Emotion',
     image: './images/angry.jpg',
   },
