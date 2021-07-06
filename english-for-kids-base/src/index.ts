fd772357b7016cdc98d988e029b215919f10ac44
Alexander Huracan <Nokia2611@mail.ru>
2021-07-06 03:56:43 +0300

    feat: add mode switcher logic

--- english-for-kids-base/src/index.ts
+++ english-for-kids-base/src/index.ts
@@ -2,8 +2,29 @@ import { headerRender } from './components/header/header';
 import { routing } from './routing';
 import './style.scss';
 
-localStorage.setItem('gameCategory', 'Animals');
-localStorage.setItem('gameDifficulty', '4x4');
-
 headerRender();
 routing();
+
+const modeSwitcher = document.getElementById('mode-switcher') as HTMLElement;
+const modeSwitcherToggle = document.querySelector(
+  '.mode-switch__toggle',
+) as HTMLInputElement;
+let mode;
+
+if (localStorage.getItem('mode') === 'game') {
+  modeSwitcherToggle.checked = true;
+}
+
+modeSwitcher.addEventListener('change', () => {
+  mode = localStorage.getItem('mode');
+  switch (mode) {
+    case 'game':
+      localStorage.setItem('mode', 'training');
+      break;
+    case 'training':
+      localStorage.setItem('mode', 'game');
+      break;
+    default:
+      break;
+  }
+});
