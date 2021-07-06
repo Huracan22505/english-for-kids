f12d33604335957450d93f8caef574ed1ad3e8be
Alexander Huracan <Nokia2611@mail.ru>
2021-07-06 10:55:20 +0300

    fix: local storage

--- english-for-kids-base/src/index.ts
+++ english-for-kids-base/src/index.ts
@@ -9,7 +9,10 @@ const modeSwitcher = document.getElementById('mode-switcher') as HTMLElement;
 const modeSwitcherToggle = document.querySelector(
   '.mode-switch__toggle',
 ) as HTMLInputElement;
+
 let mode;
+
+if (!localStorage.getItem('mode')) localStorage.setItem('mode', 'training');
 
 if (localStorage.getItem('mode') === 'game') {
   modeSwitcherToggle.checked = true;
