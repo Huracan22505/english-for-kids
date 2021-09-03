3c21601ea9892de146d84b85ce867d9b68c9d55e
Huracan22505 <nokia2611@mail.ru>
2021-09-03 12:57:04 +0300

    refactor: remove footer

--- english-for-kids-base/src/app.ts
+++ english-for-kids-base/src/app.ts
@@ -2,7 +2,6 @@ import refs from './shared/refs';
 import { menuHandler, headerMarkup } from './components/header/header';
 import { backdropMarkup } from './components/backdrop/backdrop';
 import { sidebarMarkup } from './components/sidebar/sidebar';
-import { footerMarkup } from './components/footer/footer';
 import { routing } from './routing';
 import { setupLocalStorage } from './shared/utils/localStorage';
 
@@ -16,7 +15,6 @@ const appMarkup = `
     ${backdropMarkup}
     <main id="main"></main>
     ${sidebarMarkup}
-    ${footerMarkup}
 `;
 
 export const appRender = (): void => {
